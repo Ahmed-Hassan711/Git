985c2a48e4fb2449fcbce9aae79d58009bd027ca
Ahmed Hassan <ahmed@debian.debian.com>
2023-02-16 15:57:48 +0500

    Advance Server

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
 
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-express().listen(5000);
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
